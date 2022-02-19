b7274a1cd7956d9f5172b6b74009dcdadcc3d025
wuliangjian <15929560520@163.com>
2022-02-19 19:43:57 +0800

    修改my页面

--- src/pages/tabBar/my/index.js
+++ src/pages/tabBar/my/index.js
@@ -4,11 +4,23 @@ const app = getApp()
 
 Page({
   data: {
-    
+    userinfo: {}
   },
 
   onLoad() {
   
   },
 
+  getuserinfo(res) {
+    wx.getUserProfile({
+      desc: '用于展示昵称头像',
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userinfo: res.userInfo
+        })
+      }
+    })
+    
+  }
 })
